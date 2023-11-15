23c9a22868faf9c7c3dae2c33687b45356cae331
Ashrafkhan Azamov <aazamov809@gmail.com>
2023-11-15 18:49:12 +0500

    hf

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -3,11 +3,25 @@
 import { useGetUserByIdQuery } from "@/redux/services/userApi";
 import Link from "next/link";
 
-const page = ({ params }) => {
-  const { data, error, isLoading, isFetching } = useGetUserByIdQuery(params);
+const page = ({ params }: { params: any }) => {
+  const { data } = useGetUserByIdQuery(params);
+
   console.log(data);
 
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        {!data && <p>there is no data</p>}
+        {data && (
+          <div>
+            {data.map((user) => (
+              <p>{user.title}</p>
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default page;
